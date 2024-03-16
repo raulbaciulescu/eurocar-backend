b5072a2d217280172052f49dff2dcf1bf72cb549
raulbaciulescu <raulbaciulescu@gmail.com>
2024-03-16 23:37:21 +0200

    first commit

---
+++ src/main/java/com/eurocar/EurocarBackendApplication.java
@@ -0,0 +1,13 @@
+package com.eurocar;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EurocarBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EurocarBackendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/eurocar/EurocarBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.eurocar;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurocarBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
